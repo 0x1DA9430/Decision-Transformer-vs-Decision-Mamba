2adb44b7472e333c109b6e2e3aaa5b46a1ffe732
Ke Yan <yank.fitzgerald@gmail.com>
2024-06-13 03:02:21 +0100

    update script

--- atari/run_on_eddie.sh
+++ atari/run_on_eddie.sh
@@ -2,10 +2,13 @@
 
 # Grid Engine options (lines prefixed with #$)
 # Runtime limit of 1 hour:
+$ -N test_run
+$ -cwd
 $ -l h_rt=12:00:00
+$ -l h_vmem=24G
 #
 # Set working directory to the directory where the job is submitted from:
-$ -cwd
+
 #
 # Request one GPU in the gpu queue:
 $ -q gpu&nbsp;
@@ -14,10 +17,10 @@ $ -pe gpu-a100 1
 # Request 4 GB system RAM&nbsp;
 # the total system RAM available to the job is the value specified here multiplied by&nbsp;
 # the number of requested GPUs (above)
-$ -l h_vmem=24G
+
 
 # Initialise the environment modules and load CUDA version 11.0.2
-# . /etc/profile.d/modules.sh
+. /etc/profile.d/modules.sh
 # module load cuda/12.1.1
 module load anaconda
 
