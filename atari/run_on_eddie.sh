8478b82b7e69c6a1511abd89b23e6014539aa5d9
Ke Yan <yank.fitzgerald@gmail.com>
2024-06-13 03:19:44 +0100

    update script

--- atari/run_on_eddie.sh
+++ atari/run_on_eddie.sh
@@ -3,13 +3,13 @@
 # Grid Engine options (lines prefixed with #$)
 #$ -N test_run
 #$ -cwd
-#$ -l h_rt=12:00:00
-#$ -l h_vmem=24G
+#$ -l h_rt=1:00:00
 
 
 # Request one GPU in the gpu queue:
-#$ -q gpu&nbsp;
+#$ -q gpu
 #$ -pe gpu-a100 1
+#$ -l h_vmem=24G
 
 
 # Initialise the environment modules and load CUDA version 11.0.2
