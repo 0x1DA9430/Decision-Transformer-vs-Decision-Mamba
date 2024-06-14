b3c8e9ebad3e9419a8c740eef67de8f32c9806bd
Ke Yan <yank.fitzgerald@gmail.com>
2024-06-14 16:11:25 +0100

    update scripts

--- atari/run_atari_2.sh
+++ atari/run_atari_2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DATA_DIR=./data/data_atari/
-OUT_DIR=./output/atari_2/
+OUT_DIR=./output/atari_2_eddie/
 
 # EXP_P=dmamba_pong
 # for seed in 123; do python train_atari.py --game 'Pong' --data_dir_prefix $DATA_DIR --context_length 50 --token_mixer 'mamba' --output $OUT_DIR --experiment $EXP_P --seed $seed; done
@@ -17,11 +17,11 @@ EXP_DTP=dtrans_pong
 for seed in 123 321 213; do python train_atari.py --game 'Pong' --data_dir_prefix $DATA_DIR --context_length 50 --token_mixer 'attn' --output $OUT_DIR --experiment $EXP_DTP --seed $seed; done
 
 
-# EXP_S=dmamba_seaquest
-# for seed in 123 321 213; do python train_atari.py --game 'Seaquest' --data_dir_prefix $DATA_DIR --context_length 30 --token_mixer 'mamba' --output $OUT_DIR --experiment $EXP_S --seed $seed; done
+EXP_S=dmamba_seaquest
+for seed in 123 321 213; do python train_atari.py --game 'Seaquest' --data_dir_prefix $DATA_DIR --context_length 30 --token_mixer 'mamba' --output $OUT_DIR --experiment $EXP_S --seed $seed; done
 
-# EXP_DTS=dtrans_seaquest
-# for seed in 123 321 213; do python train_atari.py --game 'Seaquest' --data_dir_prefix $DATA_DIR --context_length 30 --token_mixer 'attn' --output $OUT_DIR --experiment $EXP_DTS --seed $seed; done
+EXP_DTS=dtrans_seaquest
+for seed in 123 321 213; do python train_atari.py --game 'Seaquest' --data_dir_prefix $DATA_DIR --context_length 30 --token_mixer 'attn' --output $OUT_DIR --experiment $EXP_DTS --seed $seed; done
 
 
 
--- atari/run_on_eddie.sh
+++ atari/run_on_eddie.sh
@@ -9,16 +9,16 @@
 # Request one GPU in the gpu queue:
 #$ -q gpu
 #$ -pe gpu-a100 1
-#$ -l h_vmem=24G
+#$ -l h_vmem=40G
 
 
 # Initialise the environment
 . /etc/profile.d/modules.sh
-module load cuda
-module load anaconda
+module load cuda/12
+module load anaconda/2024
 
-conda activate ssm2
+conda activate ssm
 python -m atari_py.import_roms ROMS
 
 # Run the executable
-./run_atari_2.sh > run_atari_2.log 2>&1
+bash run_atari_2.sh > run_atari_2.log 2>&1
