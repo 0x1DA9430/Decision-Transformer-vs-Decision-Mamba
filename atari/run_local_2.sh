a86ba8c14952933ae5af6d56e28daf8b965279f5
Ke Yan <yank.fitzgerald@gmail.com>
2024-07-19 14:19:27 +0100

    update

--- atari/run_local_2.sh
+++ atari/run_local_2.sh
@@ -1,6 +1,6 @@
 # Run the executable
 DATA_DIR=./data/data_atari/
-OUT_DIR=./output/context_10_rtg_5max_action_fusion_simplest/atari_kungfumaster_local/
+OUT_DIR=./output/context_10_rtg_5max_action_fusion_opt1p/atari_kungfumaster_local/
 
 EXP_Q=dmamba_kungfumaster
 for seed in 123 132 321; do python train_atari.py --game 'KungFuMaster' --data_dir_prefix $DATA_DIR --context_length 10 --token_mixer 'mamba' --output $OUT_DIR --experiment $EXP_Q --seed $seed --use_action_fusion; done
